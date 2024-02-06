6353694578ca1f842fdd2d932bc343c265b5ffee
Fun_Dan3 <dfr34560@gmail.com>
2024-02-06 16:29:28 +0300

    Added help and exit commands

--- client/main.py
+++ client/main.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
+#I am rushing to finish this project because It needs to be done week before I started writing this file
 
 import EchoAPI
 import SymetricEncryptionLayer as SEL
@@ -8,6 +9,7 @@ import pickle
 import os
 import asyncio
 import hashlib
+import aioconsole
 
 def hash(data, algorithm = None):
 	if not algorithm:
@@ -70,9 +72,37 @@ print(client.read_banner())
 login_prompt()
 messages = read_message_file(client.password)
 
+async def console_prompt():
+	help_message = """
+help - see this menu
+exit - exit messager
+read <optional: quantity> - read <quantity> messages. if quantity isnt specified then all messages will be displayed
+profile <positional: username> - display username and publick hash of the user.
+message <positional: username> - start typing message to <username>. You should check recipient public hash using profile command to validate their identity.
+"""
+	user_input = await aioconsole.ainput(f"{client.user.username}:{client.user.public_hash} > ")
+	arguments = user_input.split(" ")
+	command = arguments[0]; arguments.pop(0)
+	if command not in "help;exit;read;profile;message".split(";"):
+		print(f"Unknown command '{command}'")
+		return
+	if command == "help":
+		if arguments:
+			print("Command 'help' doesnt take any arguments")
+			return
+		print(help_message)
+	if command == "exit":
+		if arguments:
+			print("Command 'exit' doesnt take any arguments")
+			return
+		raise KeyboardInterrupt("Program finished")
+
 @client.event.on_ready()
 async def on_ready():
-	return
+	print("You are connected. Developer of this messager is a failure at GUIs so currently only console is supported\nTo see help type 'help'")
+	while True:
+		print(f"You have {len(messages)} unread messages.")
+		await console_prompt()
 
 @client.event.on_message()
 async def on_message(message):
