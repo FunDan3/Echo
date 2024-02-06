42c82a2688ec4d03fb4c1fb481923f1e442e70cf
Fun_Dan3 <dfr34560@gmail.com>
2024-02-06 15:41:01 +0300

    Added support for importing cryptodata generated by keygen.py

--- client/main.py
+++ client/main.py
@@ -1,10 +1,11 @@
 #! /usr/bin/python3
 
 import EchoAPI
+import pickle
 import os
 
 client = EchoAPI.client("http://127.0.0.1:8080")
-
+print(client.read_banner())
 def login_prompt():
 	if not os.path.exists("./container.epickle"):
 		print("Container not found. Starting registration process")
@@ -12,9 +13,15 @@ def login_prompt():
 			try:
 				nickname = input("Nickname: ")
 				password = input("Password: ")
-				client.register(nickname, password)
+				if not os.path.exists("./cryptodata.pickle"):
+					print("No cryptodata file found. Generating random one...")
+					container = client.register(nickname, password)
+				else:
+					print("Cryptodata file found. Importing...")
+					with open("./cryptodata.pickle", "rb") as cryptodata_file:
+						container = client.register(nickname, password, cryptodata = pickle.loads(cryptodata_file.read()))
 				with open("./container.epickle", "wb") as container_file:
-					container_file.write(client.generate_container())
+					container_file.write(container)
 					break
 			except EchoAPI.Exceptions.InvalidRequestException as e:
 				print(f"{e}, try again")
@@ -27,6 +34,10 @@ def login_prompt():
 					break
 			except ValueError:
 				print("Password is invalid. Try again.")
+
+@client.event.on_ready()
+async def on_ready():
+	return
 
 login_prompt()
 client.start()
