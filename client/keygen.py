47089dcd18531cca4a12b676c3f847a01b652c1b
Fun_Dan3 <dfr34560@gmail.com>
2024-02-08 01:58:53 +0300

    Remade keygen a little

--- client/keygen.py
+++ client/keygen.py
@@ -11,8 +11,12 @@ import time
 
 colorama.init()
 key_start = input("How do you want to start your verification hash: ")
+time_started = time.time()
+hashed = 0
+cpu_count = mp.cpu_count()
 
-def gencrypto():
+def gencrypto(core):
+	global hashed
 	client = EchoAPI.client("")
 	while not os.path.exists("./cryptodata.pickle"):
 		public_key, private_key, public_sign, private_sign = client.generate_cryptodata()
@@ -20,22 +24,17 @@ def gencrypto():
 		verification_hash.update(public_key)
 		verification_hash.update(public_sign)
 		hash = base64.b64encode(verification_hash.digest()).decode("utf-8").replace("=", "")
-		toprint = colorama.Fore.LIGHTGREEN_EX
-		for i in range(len(key_start)):
-			if hash[i] != key_start[i]:
-				toprint += colorama.Fore.LIGHTRED_EX
-				toprint += hash[i:]
-				break
-			toprint += hash[i]
-		print(toprint+colorama.Style.RESET_ALL)
+		hashed += 1
+		if hashed % 5000 == 0 and core == 0:
+			print(f"{round(hashed/(time.time()-time_started)*cpu_count/1000, 1)} kH/S")
 		if hash.startswith(key_start):
 			with open("./cryptodata.pickle", "wb") as f:
 				f.write(pickle.dumps((public_key, private_key, public_sign, private_sign)))
 			break
 
 processes = []
-for core in range(0, mp.cpu_count()):
-	processes.append(mp.Process(target = gencrypto))
+for core in range(0, cpu_count):
+	processes.append(mp.Process(target = gencrypto, args = [core]))
 for process in processes:
 	process.start()
 for process in processes:
