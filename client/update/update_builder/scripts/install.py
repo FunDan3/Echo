67204d508a1ce790dbff65175bd382ef2271592f
Fun_Dan3 <dfr34560@gmail.com>
2024-02-17 16:46:37 +0300

    Changed updater script

---
+++ client/update/update_builder/scripts/install.py
@@ -0,0 +1,48 @@
+import os
+
+def remove(path):
+	global remove
+	if os.path.isdir(path):
+		path = path + ("/" if not path.endswith("/") else "")
+		if os.path.exists(path):
+			for file in os.listdir(path):
+				file_path = path+file
+				remove(file_path)
+			os.rmdir(path)
+			print(f"removed {path}")
+		else:
+			print(f"{path} doesnt exist so werent removed")
+
+	else:
+		if os.path.exists(path):
+			os.remove(path)
+			print(f"removed {path}")
+		else:
+			print(f"{path} doesnt exist so werent removed")
+
+def write(path, data):
+	previous_directory = ""
+	for directory in path.split("/")[:path.count("/")]:
+		if not os.path.exists(previous_directory+directory):
+			os.mkdir(previous_directory+directory)
+			print(f"created directory {previous_directory}{directory}/")
+		previous_directory+=f"{directory}/"
+	if os.path.exists(path):
+		with open(path, "rb") as read:
+			read_data = read.read()
+		if read_data != data:
+			with open(path, "wb") as f:
+				f.write(data)
+			print(f"written {path}")
+		else:
+			print(f"{path} already exists and updated.")
+	else:
+		with open(path, "wb") as f:
+			f.write(data)
+
+
+for path in to_remove:
+	remove(path)
+
+for path in to_write:
+	write(path, update_files[path])
